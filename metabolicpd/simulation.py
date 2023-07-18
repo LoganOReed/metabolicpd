9bb2f1b192e0dcc59ad435fef5f18d79e2d7a721
orlogan <orlogan7@gmail.com>
2023-07-18 13:08:05 -0400

    Implemented S Matrix

--- metabolicpd/simulation.py
+++ metabolicpd/simulation.py
@@ -12,7 +12,9 @@ class LIFE_Network:
 
     # TODO: add reasonable file checking and exceptions
     def __init__(self, file):
+        # read graph/network from clean file
         self.network = pd.read_excel(file)
+
         unique_entries = np.unique(self.network[["tail", "head"]].values)
 
         metabolites = []
@@ -44,78 +46,93 @@ class LIFE_Network:
             "type": metabolite_types,
         }
         self.df = pd.DataFrame(temp_dict)
+        # initialize random masses for metabolites
         np.random.seed(seed=12)
-        self.df = self.df.assign(vals=np.random.rand(len(self.df.index)))
+        self.curr_mass = np.random.rand(len(self.df.index))
+        self.df = self.df.assign(mass=self.curr_mass)
 
+        self.create_S_matrix()
 
-def extract_metabolites(data_frame):
-    """Extracts the unique names within the network dataframe.
+    # NOTE: I've set this up so ideally it will only be called by the "simulation" function once written
+    def create_S_matrix(self):
+        """Create the 'S' matrix, representing the dynamics for the network x' = S(x) * f.
 
-    This function simply return all the unique names from an edge list
-    held in a pandas dataframe. If an entry in the df has multiple string (separated
-    by a comma and space: ", "), then it splits the entry.
+        The form of the 'S' matrix comes follows LIFE dynamics, with edges in the network corresponding to columns in the
+        matrix and metabolites in the network corresponding to the rows of the matrix.
 
-    Args:
-        data_frame : pandas dataframe that holds and edge list
+        Returns:
+            A numpy array representing the S matrix for the current metabolite masses.
+        """
+        s_matrix = []
+        edge_columns_df = self.network[
+            ["tail", "head", "uber"]
+        ]  # get a dataframe for just the edges (future-proof for uberedges)
+        for row in edge_columns_df.itertuples():
+            # iterate through each of the edges
+            # there could be more than one substrate
+            # or product! (for instance, in a hyperedge)
+            substrates = row.tail.split(", ")
+            products = row.head.split(", ")
+            uber_modulators = row.uber.split(", ")
 
-    Returns:
-        A DataFrame that contains the unique metabolites, their unique index, and their type
-    """
-    unique_entries = np.unique(data_frame[["tail", "head"]].values)
+            col = np.zeros(self.df.shape[0])
 
-    metabolites = []
-    for entry in unique_entries:
-        elements = entry.split(", ")
-        for ele in elements:
-            metabolites.append(ele)
-    new_unique_metabolites = list(dict.fromkeys(metabolites))
+            # build the uber term for each expression, default if no uber edges is 1
+            uber_term = 1.0
+            # TODO Ask Chris if we mean to modify the initial uber_term=1 by multiple metabolites
+            #      It seems like it's accounting for the various weights of each + and - on an edge
+            #      But I still don't fully understand the definition of the S matrix we are using
+            for uber in uber_modulators:
+                # there should probably be some checking that happens so we don't duplicate the code in the if statements
+                if uber[-1] == "+":  # check the last term in the entry, enhancer
+                    # TODO Warning this feels very clunky and potentially slow
+                    idx = self.df.loc[self.df["name"] == uber[:-2], "index"].to_numpy()[
+                        0
+                    ]
+                    # note uber_term will always be greater than one
+                    uber_term = uber_term * np.exp(
+                        self.curr_mass[idx] / (self.curr_mass[idx] + 1)
+                    )
+                elif uber[-1] == "-":  # check the last term in the entry, enhancer
+                    # TODO Warning this feels very clunky and potentially slow
+                    idx = self.df.loc[self.df["name"] == uber[:-2], "index"].to_numpy()[
+                        0
+                    ]
+                    # note uber_term will always be less than one
+                    uber_term = uber_term / np.exp(
+                        self.curr_mass[idx] / (self.curr_mass[idx] + 1)
+                    )
 
-    metabolite_types = []
-    for ele in new_unique_metabolites:
-        # Using regular expression strings to identify source or sink terms in metabolite dictionary keys
-        sink_res = re.search("^[e]+[0-9]$", ele)  # starts with e, ends with #...
-        source_res = re.search("^[s]+[0-9]$", ele)  # starts with s, end with #...
-        if (
-            sink_res is not None
-        ):  # check if we found something in the entry that matches the format of 'e###'
-            metabolite_types.append("sink")
-        elif (
-            source_res is not None
-        ):  # chick if we found something that matches format of source term
-            metabolite_types.append("source")
-        else:  # if we didn't find a source or sink term, then it must be an actual metabolite!
-            metabolite_types.append("actual")
-
-    temp_dict = {
-        "index": [i for i in range(0, len(new_unique_metabolites))],
-        "name": new_unique_metabolites,
-        "type": metabolite_types,
-    }
-
-    metabolite_data_frame = pd.DataFrame(temp_dict)
-
-    return metabolite_data_frame
-
-
-def create_random_values(df):
-    """Creates a numeric array based on number of actual metabolites.
-
-    Args:
-        metabolite_names: a list of metabolites in network
-
-    Returns:
-        Numpy array of random values between 0, 1 that is in parallel with sym_array
-    """
-    np.random.seed(seed=12)
-    return df.assign(vals=np.random.rand(len(df.index)))
+            # Note that I'm vectorizing as much as possible as the actual dataframe will be massive.
+            idxs = self.df.loc[self.df["name"].isin(substrates), "index"]
+            idxp = self.df.loc[self.df["name"].isin(products), "index"]
+            # Case: Hyperedge
+            if len(substrates) > 1 or len(products) > 1:
+                # This chunk of code finds min, and sets col values for both substrates and products appropriately
+                min_sub = np.min(self.curr_mass[idxs])
+                col[idxs] = -1 * min_sub * uber_term
+                col[idxp] = min_sub * uber_term
+            # Case: Not Hyperedge
+            else:
+                if (
+                    self.df.loc[self.df["name"] == substrates[0], "type"].item()
+                    == "source"
+                ):
+                    col[idxp] = 1
+                elif (
+                    self.df.loc[self.df["name"] == products[0], "type"].item() == "sink"
+                ):
+                    col[idxs] = -1 * self.curr_mass[idxs] * uber_term
+                else:
+                    col[idxs] = -1 * self.curr_mass[idxs] * uber_term
+                    col[idxp] = self.curr_mass[idxs] * uber_term
+            s_matrix.append(col)
+            # Seems to be the most sane way to check this code
+            print(pd.DataFrame(col).to_markdown())
+        self.s_matrix = s_matrix
 
 
 if __name__ == "__main__":
     network = LIFE_Network("data/simple_pd_network.xlsx")
-    print(type(network.df))
-    print(network.df)
-    # metabolites = create_random_values(metabolites)
-    # print(metabolites)
-
-    # print(metabolite_types)
-    # print("index of 's6' "+str(unique_metabolites.index("a_syn_0")))
+    print(network.curr_mass)
+    print(network.df.to_markdown())
--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -14,6 +14,19 @@ def test_df_type():
 def test_df_data():
     """Test DataFrame Schema"""
     network = LIFE_Network("data/simple_pd_network.xlsx")
-    schema_columns = ["index", "name", "type", "vals"]
+    schema_columns = ["index", "name", "type", "mass"]
     # print("index of 's6' "+str(unique_metabolites.index("a_syn_0")))
     assert network.df.columns.tolist() == schema_columns
+
+
+def test_df_rows():
+    """Test for expected number of rows."""
+    network = LIFE_Network("data/simple_pd_network.xlsx")
+    assert network.df.shape[0] == 37
+
+
+def test_network_edges():
+    """Test for expected number of edges."""
+    network = LIFE_Network("data/simple_pd_network.xlsx")
+    num_edges = len(network.network["tail"].values)  # the number of cols in the matrix
+    assert num_edges == 28
