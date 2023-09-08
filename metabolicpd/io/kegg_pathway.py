2659272a9d9f9143f44949a79cfa2085eedca60f
Logan Reed <me@loganreed.org>
2023-09-08 06:41:45 -0400

    Implemented Basic API Interface

--- metabolicpd/io/kegg_pathway.py
+++ metabolicpd/io/kegg_pathway.py
@@ -1,4 +1,17 @@
-import Bio as bp
+import os
 
-if "__name__" == "__main__":
-    print(bp.__version__)
+import Bio.KEGG.REST as bp
+from Bio.KEGG.KGML import KGML_parser
+
+out_dir = "data/kegg"
+kegg_id = "mtu01200"
+
+if __name__ == "__main__":
+    kgml_path = out_dir + "/" + kegg_id + ".xml"
+    if not os.path.isfile(kgml_path):
+        print("Grabbing KGML using API...")
+        with open(kgml_path, "w") as f:
+            f.write(bp.kegg_get(kegg_id, "kgml").read())
+    with open(kgml_path) as f:
+        pathway = KGML_parser.read(f)
+    print(pathway)
