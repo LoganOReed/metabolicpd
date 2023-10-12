7f5942100cacc9b52dab3199216a0e98a6049370
Logan Reed <me@loganreed.org>
2023-10-12 01:13:20 -0400

    Added Small Updates to Kegg IO

--- metabolicpd/io/kegg_pathway.py
+++ metabolicpd/io/kegg_pathway.py
@@ -8,6 +8,13 @@ from Bio.KEGG.KGML import KGML_parser
 out_dir = "data/kegg"
 # Carbon metabolism - Mycobacterium tuberculosis H37Rv
 kegg_id = "mtu01200"
+# kegg_id = "hsa05165"
+
+
+def get_compound_name(cid):
+    """Uses KEGG REST API to convert a kegg compound id (`cpd:C#####` or `C#####`) to a list of names."""
+    return bp.kegg_find("compound", cid).read()
+
 
 if __name__ == "__main__":
     kgml_path = out_dir + "/" + kegg_id + ".xml"
@@ -22,11 +29,26 @@ if __name__ == "__main__":
         print("Could not open/read file: ", kgml_path)
         sys.exit(1)
 
+    # Used to list inhibitors
+    # for g in pathway.relations:
+    #     if g.subtypes[0][0] == "inhibition":
+    #         print(g)
+
     # print pathway info
-    for g in pathway.genes:
+    cpd = "cpd:C00085 cpd:C05345"
+    for g in pathway.reactions:
         print(g)
+        for h in g.products:
+            print(h.name)
     print("####################")
-    for g in pathway.reaction_entries:
-        print(g)
-    print(pathway)
+
+    cpds = []
+    for g in pathway.compounds:
+        cpds = cpds + g.name.split()
+
+    cpd_path = out_dir + "/" + kegg_id + ".compounds"
+    cpd_file = open(cpd_path, "w")
+    for c in cpds:
+        cpd_file.write(c[4:] + "\n")
+
     print("#######################")
